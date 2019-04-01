35016afe540fe8ff14ba9050299e376531ad436a
lzh <1581627402@qq.com>
2019-04-01 19:18:07 +0800

    lzh

--- machine_learning/test_dot.py
+++ machine_learning/test_dot.py
@@ -1,10 +1,9 @@
 # coding=utf-8
-import numpy as np
-theta = np.array([[1],
-                  [2],
-                  [3]])#(3,1)
-X=np.array([[1,1,1,1],     #(3,4)
-            [2,2,2,1],
-            [3,3,3,1]
-                    ])
-print(X.sum(axis=0),X)
+import sys
+sys.path.append('/home/csdn/AID/lzh/decorate')
+from mydecorate import Decorater
+@Decorater
+def pri():
+    print("test_dot")
+
+pri()
