4ff48bc5a7079a1379ab4f41d6dff69d3e2b26f5
lzh <1581627402@qq.com>
2019-04-24 20:01:59 +0800

    lzh

--- poem/poems.py
+++ poem/poems.py
@@ -29,5 +29,41 @@ def process_poems(file_name):
     #按诗的字数排序
     print(poems)
     poems = sorted(poems,key = lambda line:len(line))
-    print(poems)
+    all_words = []
+    for poem in poems:
+        all_words+=poem
+    counter = collections.Counter(all_words)
+    count_pairs = sorted(counter.items(),key=lambda x:-x[-1])
+    words,_=zip(*count_pairs)
+
+    words = words[:len(words)]+("",)
+
+    words_int_map=dict(zip(words,range(len(words))))
+    poems_vector = [list(map(lambda word:words_int_map.get(word,len(words)),poem))for poem in poems]
+    print(poems_vector[1])
+    print(np.array(poems_vector).shape)
+    return poems_vector,words_int_map,words
+
+def generate_batch(batch_size,poems_vec,word_to_int):
+    n_chunk=len(poems_vec)//batch_size
+    x_batches=[]
+    y_batches=[]
+
+    for i in range(n_chunk):
+        start_index=i*batch_size
+        end_index = start_index+batch_size
+        batches=poems_vec[start_index:end_index]
+        #找到最长的长度
+        length=max(map(len,batches))
+        x_data=np.full((batch_size,length),word_to_int[''],np.int32)
+
+        for row in range(batch_size):
+            x_data[row,:len(batches[row])]=batches[row]
+        y_data=np.copy(x_data)
+        y_data[:,:-1]=x_data[:,1:]
+        x_batches.append(x_data)
+        y_batches.append(y_data)
+    return x_batches,y_batches
+    pass
+
 process_poems("./data/poetry.txt")
